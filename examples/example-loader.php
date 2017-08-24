6a607264e2b9a596041692a2aebacc2162d6f65e
j.faassen <j.faassen@linkorb.com>
2017-08-24 14:01:25 +0200

    Fix example

--- examples/example-loader.php
+++ examples/example-loader.php
@@ -9,7 +9,7 @@ use Pipeline\Loader\YamlLoader;
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $loader = new YamlLoader();
-$pipeline = $loader->loadFile(__DIR__ . '/demo.pipeline.yml');
+$pipeline = $loader->loadFile(__DIR__ . '/bbc-news-demo.pipeline.yml');
 
 $job = new Job($pipeline);
 $job->setVariable('topic', 'world');
